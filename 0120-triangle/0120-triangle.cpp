0e6c0729e5c129acb76dda928494fd12233e4913
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2024-12-03 16:40:26 +0200

    Time: 18 ms (5.06%), Space: 31.7 MB (5.29%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,14 @@
-// Author: Mohamed Khedr
 class Solution {
 public:
-    /*
-    - this a simple recursive approach: just by trying all the different paths
-    we took the current element in the current row and add it to the minimum between the two choices:
-    either index j or index j + 1 on the next row.
-    - and by adding memoization -> the time complexity is O(N^2);
-    */
     vector<vector<int>> dp;
-    int solve(int i, int j, vector<vector<int>> &triangle) {
-        if (i == triangle.size())
-            return 0;
+    int solve(int i, int j, vector<vector<int>>& triangle) {
+        if(i == triangle.size()) return 0;
         int &ret = dp[i][j];
-        if (~ret)
-            return ret;
-        return ret = triangle[i][j] + min(solve(i + 1, j, triangle), solve(i + 1, j + 1, triangle));
-    }
-    int minimumTotal(vector<vector<int>> &triangle) {
-        dp = vector<vector<int>>(200, vector<int>(200, -1));
-        return solve(0, 0, triangle);
+        if(~ret) return ret;
+        return ret = triangle[i][j] + min(solve(i+1, j, triangle), solve(i+1,j+1, triangle));
+    } 
+    int minimumTotal(vector<vector<int>>& triangle) {
+        dp = vector<vector<int>> (300, vector<int>(300, -1));
+        return solve(0,0, triangle);
     }
 };
