afd1e4db1a9a39706b6e59bf4a05b632288d7f84
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-09 06:44:27 +0200

    Time: 90 ms (8.79%), Space: 114.3 MB (11.51%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-int p[100001];
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        for(int i=1;i<=k-1;i++) colors.push_back(colors[i-1]);
         int n = colors.size();
-        // vector<int> p(n+1);
-        p[1] = (colors[0] != colors[n-1]);
+        vector<int> p(n+1);
         for(int i=2;i<=n;i++) {
-            p[i] = p[i-1] + (colors[i-1] != colors[i-2]);
+            p[i] = p[i-1] + (colors[i-1] ^ colors[i-2]);
         }
-        for(int i=1;i<=n;i++) cout << p[i] << ' ';
         int l = 1, r = k;
         int ans = 0;
         while(r <= n) {
             ans += ((p[r] - p[l]) == k-1);
             l++, r++;
-        }
-        for(int i=1;i<=k-1;i++) {
-            int before = p[n] - p[n-i+1], after = p[k-i];
-            ans += ((before+after) == k-1);
         }
         return ans;
     }
