a62e77ec2dc3415c8aa0275fa94aee48c0bcd882
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-09 20:01:16 +0200

    Time: 0 ms (100%), Space: 35.3 MB (81.86%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.cpp
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    bool vis[100001];  
-    int solve(int i, vector<int> &arr) {
-        if(!arr[i]) return 1;
-        vis[i] = 1;
-        int mx = 0;
-        if(i+arr[i] < arr.size() && !vis[i+arr[i]]) mx = max(mx, solve(i+arr[i], arr) );
-        if(i-arr[i] >= 0 && !vis[i-arr[i]]) mx = max(mx, solve(i-arr[i], arr));
-        return mx;
-    }
     bool canReach(vector<int>& arr, int start) {
-        return solve(start, arr);
+        queue<int> q;
+        vector<bool> vis(arr.size());
+        q.push(start);
+        bool ok = 0;
+        while(!q.empty()) {
+            int x = q.front();
+            if(arr[x] == 0) {
+                ok = 1;
+                break;
+            }
+            q.pop();
+            if(x+arr[x] < arr.size() &&!vis[x+arr[x]]) q.push(x+arr[x]), vis[x+arr[x]] = 1;
+            if(x-arr[x] >= 0 && !vis[x-arr[x]]) q.push(x-arr[x]), vis[x-arr[x]] = 1;
+        }
+        return ok;
     }
 };
