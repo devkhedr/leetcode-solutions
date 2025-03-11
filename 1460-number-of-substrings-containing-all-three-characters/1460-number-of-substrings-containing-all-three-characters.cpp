565a46e530ff6d0901edfd0f88a9d18adb0dadd9
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-11 22:58:25 +0200

    Time: 0 ms (100%), Space: 11 MB (81.01%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int last_a = -1 , last_b = -1, last_c = -1, n = s.size(), ans = 0;
+        int nearest_a = -1 , nearest_b = -1, nearest_c = -1, n = s.size(), ans = 0;
         for(int i=n-1;i>=0;i--) {
             if(s[i] == 'a') {
-                if(last_b != -1 && last_c != -1) {
-                    ans += n-max(last_b, last_c);
+                if(nearest_b != -1 && nearest_c != -1) {
+                    ans += n-max(nearest_b, nearest_c);
                 }
-                last_a = i;
+                nearest_a = i;
             }
             else if(s[i] == 'b') {
-                if(last_a != -1 && last_c != -1) {
-                    ans += n-max(last_a, last_c);
+                if(nearest_a != -1 && nearest_c != -1) {
+                    ans += n-max(nearest_a, nearest_c);
                 }
-                last_b = i;
+                nearest_b = i;
             }
             else {
-                if(last_a != -1 && last_b != -1) {
-                    ans += n-max(last_a, last_b);
+                if(nearest_a != -1 && nearest_b != -1) {
+                    ans += n-max(nearest_a, nearest_b);
                 }
-                last_c = i;
+                nearest_c = i;
             }
         }
         return ans;
