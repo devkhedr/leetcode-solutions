944e645e0ab7eec387932f4820b50566c9212675
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-14 04:17:43 +0200

    Time: 45 ms (19.54%), Space: 88.2 MB (73.33%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         int n = candies.size();
-        auto check = [&] (long long  mid) {
+        auto check = [&] (int mid) {
             long long cnt = 0;
             for(int i=0;i<n;i++) {
                 cnt += candies[i] / mid;
@@ -10,11 +10,11 @@ public:
             }
             return 0;
         };
-        long long l = 1, r = 1e7, ans = 0, mid;
-        while(l <= r) {
-            mid = l + (r-l) / 2;
-            if(check(mid)) ans = mid, l = mid + 1;
-            else r=mid-1;
+        int low = 1, high = 1e7, ans = 0, mid;
+        while(low <= high) {
+            mid = low + (high-low) / 2;
+            if(check(mid)) ans = mid, low = mid + 1;
+            else high=mid-1;
         }
         return ans;
     }
