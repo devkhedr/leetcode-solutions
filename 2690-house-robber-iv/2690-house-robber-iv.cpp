5e1c8fddee724a2172097d4d2b5aaa40765cbdaf
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-15 05:30:33 +0200

    Time: 35 ms (20.78%), Space: 61 MB (40.42%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+        auto check = [&](int mid) {
+            int cnt = 0;
+            for(int i=0;i<n;i++) {
+                if(nums[i] <= mid) cnt++,i++;
+                if(cnt>=k) return 1;
+            }
+            return 0;
+        };
+        int low = 1, high = 1e9, sol = 0, mid;
+        while(low <= high) {
+            mid = low + (high - low) / 2;
+            if(check(mid)) sol = mid, high = mid - 1;
+            else low = mid + 1;
+        }
+        return sol;
+    }
+};
