bf851f1888df7d7752e5caf0e56f3bacc313c076
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-17 04:49:43 +0200

    Time: 0 ms (100%), Space: 17.2 MB (64.71%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        vector<int> v(501);
+        vector<int> v(*max_element(nums.begin(), nums.end()) + 1);
         for(int &x: nums) v[x]++;
         for(int &x: nums) {
             if(v[x]%2) return 0;
