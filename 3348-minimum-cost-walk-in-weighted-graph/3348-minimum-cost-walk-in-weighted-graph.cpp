f639861a805eff834e395af4af235a0cbbb55d6e
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-20 19:38:57 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,15 +1,11 @@
-#include <vector>
-#include <numeric>
-using namespace std;
-
 struct DSU {
     int n;
     vector<int> parent, gsize, band;   
     DSU(int n) {
         this->n = n;
-        parent.resize(n);
-        gsize.resize(n, 1);
-        band.resize(n, ~0); // Initialize to all ones (bitwise)
+        parent.resize(n+1);
+        gsize.resize(n+1, 1);
+        band.resize(n+1, 1);
         iota(parent.begin(), parent.end(), 0);
     }
     int find(int x) {
@@ -18,14 +14,17 @@ struct DSU {
     void merge(int x, int y, int val) {
         x = find(x);
         y = find(y);
-        if (x == y) {
+        if(x == y) {
             band[x] &= val;
             return;
         }
-        if (gsize[x] < gsize[y]) swap(x, y);
+        if(gsize[x] < gsize[y]) swap(x, y);
         parent[y] = x;
         gsize[x] += gsize[y];
-        band[x] &= (band[y] & val);
+        band[x] &= (val & band[y]);
+    }
+    int getSize(int x) {
+        return gsize[find(x)];
     }
     int getAnd(int x) {
         return band[find(x)];
@@ -36,16 +35,17 @@ class Solution {
 public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
         DSU s(n);
-        for (auto& it : edges) {
+        for(auto &it: edges) {
             s.merge(it[0], it[1], it[2]);
         }
         vector<int> ans;
-        for (auto& q : query) {
-            int u = q[0], v = q[1];
-            if (s.find(u) != s.find(v)) {
+        for(auto &it: query) {
+            int x = s.find(it[0]), y = s.find(it[1]);
+            if(x!=y) {
                 ans.push_back(-1);
-            } else {
-                ans.push_back(s.getAnd(u));
+            }
+            else {
+                ans.push_back(s.getAnd(x));
             }
         }
         return ans;
