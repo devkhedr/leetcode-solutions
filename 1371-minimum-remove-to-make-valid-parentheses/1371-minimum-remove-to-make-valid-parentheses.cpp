d1cb12c33096fd7c436ffca2ec2db6134f161b4c
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-03-21 04:34:21 +0200

    Time: 17 ms (78.01%), Space: 14 MB (68.1%) - LeetHub

---
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        for(int i= 0;i<s.length();i++){
+            if(s[i] == '(') st.push(i);
+            else if(s[i] == ')') {
+                if(!st.empty()) st.pop();
+                else s[i] = '*';
+            }
+        }
+        while(!st.empty()) s[st.top()] = '*', st.pop();
+        string ans="";
+        for(char &c: s) {
+            if(c != '*') ans += c;
+        }
+        return ans;
+    }
+};
