6e507b60b9966f55bd21ef7682fd9ef6c1bc5576
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-04-07 06:37:31 +0200

    Time: 75 ms (80.86%), Space: 13.3 MB (90.95%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,19 @@
-#include <vector>
-#include <cstring> 
-
-using namespace std;
-
 class Solution {
-private:
-    int sumOf(const vector<int>& nums) {
-        int sum = 0;
-        for (int num : nums)
-            sum += num;
-        return sum;
-    }
-
-    bool rec(int i, const vector<int>& nums, int n, int sum, vector<vector<int>>& dp) {
-        if (sum == 0) return true;
-        if (sum < 0 || i >= n) return false;
-        if (dp[i][sum] != -1) return dp[i][sum] == 1;
-
-        bool dont = rec(i + 1, nums, n, sum, dp);
-        bool pick = (nums[i] <= sum) ? rec(i + 1, nums, n, sum - nums[i], dp) : false;
-        
-        dp[i][sum] = (pick || dont) ? 1 : 0;
-        return pick || dont;
-    }
-
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = sumOf(nums);
-        if (sum % 2 == 1) return false;
-        sum /= 2;
-        
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return rec(0, nums, n, sum, dp);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;  // Base case: sum of 0 is always achievable
+
+        for (int num : nums) {
+            for (int j = target; j >= num; --j) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
     }
 };
