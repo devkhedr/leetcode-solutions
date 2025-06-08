3a30054db21188161351caa90e60cf67d79b6406
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-06-08 19:33:07 +0300

    Time: 433 ms (5.24%), Space: 16.4 MB (18.79%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<int> v;
-        for(int i=1;i<=n;i++) v.push_back(i);
-        sort(v.begin(), v.end(), [](int a, int b){
-            string x = to_string(a), y = to_string(b);
-            return x < y;
+        for(int i=1;i<=n;i++) {
+            v.push_back(i);
+        }
+        sort(v.begin(), v.end(), [](int x, int y) {
+            string a = to_string(x), b = to_string(y);
+            return a < b;
         });
         return v;
     }
 };
+// mohamed
+// ahmed
+
+// ahh
+// aah
+
+// aaa
+// aa
