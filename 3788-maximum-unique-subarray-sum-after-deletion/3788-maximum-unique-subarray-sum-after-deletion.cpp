09af29e07ec11e3db86aabf332ffc6fe334153b7
Mohamed Khedr <mohamed.khedr1714@gmail.com>
2025-07-25 16:51:11 +0300

    Time: 0 ms (100%), Space: 30.8 MB (5.24%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int sum = max(0, nums[0]);
-        for(int i=1;i<n;i++) {
-            if(nums[i] != nums[i-1]) {
-                sum = max(sum, sum + nums[i]);
-            }
+        sort(nums.rbegin(), nums.rend());
+        int sum = nums[0];
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i]!=nums[i-1] && nums[i]>0) sum  += nums[i];
         }
-        if(sum == 0 && nums[0]<0 && nums.back() < 0) return nums.back();
         return sum;
     }
 };
