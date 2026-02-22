1fad466d7e003df168e413ac55d4eea57a0879fc
Mohamed Khedr <mohamed.khedr.dev@gmail.com>
2026-02-22 16:54:34 +0200

    Time: 0 ms (100%), Space: 8 MB (70.75%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        int mx = 0, cnt = 0, found = 0;
+        int val = 0, mx = 0, cnt = 0;
+        bool found = 0;
         while(n > 0) {
-            int val = n%2;
+            val = n%2;
             if(!val) cnt++;
             else {
                 if(found) mx = max(mx, cnt+1);
