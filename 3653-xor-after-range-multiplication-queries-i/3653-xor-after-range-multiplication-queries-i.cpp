3b4193b618fde5d61888326371e3c402506f49f7
Mohamed Khedr <mohamed.khedr.dev@gmail.com>
2026-04-09 00:26:15 +0200

    Time: 229 ms (5.29%), Space: 246.5 MB (5.77%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -1,27 +1,59 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-    int mult(int &a, int &b) {
-        return ((1LL*a%MOD)*(b%MOD))%MOD;
-    }
+    static const int MOD = 1e9 + 7;
+
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        for(auto &x: queries) {
-            int i = x[0], j = x[1];
-            while(i <= j) {
-                nums[i] = mult(nums[i], x[3]);
-                i += x[2];
+        int n = nums.size();
+        int B = sqrt(n) + 1;
+
+        // multipliers for each index
+        vector<long long> mul(n, 1);
+
+        // group queries by step k
+        unordered_map<int, vector<vector<int>>> mp;
+        for (auto &q : queries) {
+            mp[q[2]].push_back(q);
+        }
+
+        // handle small k
+        for (auto &[k, qs] : mp) {
+            if (k <= B) {
+                vector<long long> temp(n, 1);
+
+                for (auto &q : qs) {
+                    int l = q[0], r = q[1], val = q[3];
+
+                    for (int i = l; i <= r; i += k) {
+                        temp[i] = (temp[i] * val) % MOD;
+                    }
+                }
+
+                for (int i = 0; i < n; i++) {
+                    mul[i] = (mul[i] * temp[i]) % MOD;
+                }
             }
         }
+
+        // handle large k مباشرة
+        for (auto &[k, qs] : mp) {
+            if (k > B) {
+                for (auto &q : qs) {
+                    int l = q[0], r = q[1], val = q[3];
+
+                    for (int i = l; i <= r; i += k) {
+                        mul[i] = (mul[i] * val) % MOD;
+                    }
+                }
+            }
+        }
+
+        // apply multiplication + xor
         int ans = 0;
-        for(int &x: nums) ans ^= x;
+        for (int i = 0; i < n; i++) {
+            nums[i] = (1LL * nums[i] * mul[i]) % MOD;
+            ans ^= nums[i];
+        }
+
         return ans;
     }
 };
-/*
-q = 0 2 1 4
-from 0 to 2
-0 += 1, 1 += 1
-nums[0]*=4
-nums[1]*=4
-nums[2]*=4
-*/
