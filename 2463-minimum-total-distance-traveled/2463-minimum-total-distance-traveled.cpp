59e88cab85f5d28d51d59eee27b362c0cc0ad215
Mohamed Khedr <mohamed.khedr.dev@gmail.com>
2026-04-15 06:24:38 +0200

    Time: 27 ms (65.54%), Space: 13.5 MB (75%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // long long dp[105][105];
+
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        int n = robot.size(), m = factory.size();
+        const long long INF = 1e18;
+        
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, INF));
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+
+        for (int j = 1; j <= m; j++) {
+            int pos = factory[j-1][0], limit = factory[j-1][1];
+            
+            for (int i = 0; i <= n; i++) {
+                dp[i][j] = dp[i][j-1];
+
+                long long dist = 0;
+                for (int k = 1; k <= limit && i - k >= 0; k++) {
+                    dist += abs(robot[i-k] - pos);
+                    dp[i][j] = min(dp[i][j], dp[i-k][j-1] + dist);
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
